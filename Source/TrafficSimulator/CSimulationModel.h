2cdba44d94c3e047eac7128cbb363153fd0d824a
riemervdzee <riemervdzee@gmail.com>
2012-10-16 11:37:50 +0200

    Add basic participant queue struct

--- Source/TrafficSimulator/CSimulationModel.h
+++ Source/TrafficSimulator/CSimulationModel.h
@@ -1,8 +1,14 @@
 #ifndef CSIMULATIONMODEL_H
 #define CSIMULATIONMODEL_H
 
+#include <queue>
+
+#include "TrafficDefs.h"
+
 class CNetworkView;
 class CSimulationView;
+
+class CSimulationQueueParticipant;
 
 class CSimulationModel
 {
@@ -25,6 +31,28 @@ private:
 private:
     CNetworkView    *mNetworkView;
     CSimulationView *mSimulationView;
+
+    // Queue for participants who are about to join TODO visibility?
+    std::priority_queue< CSimulationQueueParticipant> queue;
+
+    // Vector for the actual participants TODO visibility?
+    //std::vector<
+};
+
+
+class CSimulationQueueParticipant
+{
+public:
+    int time;
+    PARTICIPANTS type;
+    DIRECTION fromDirection;
+    DIRECTION toDirection;
+    int fromLane;
+    int toLane;
+
+    bool operator < ( const CSimulationQueueParticipant &a ) const {
+        return time < a.time;
+    }
 };
 
 #endif // CSIMULATIONMODEL_H
