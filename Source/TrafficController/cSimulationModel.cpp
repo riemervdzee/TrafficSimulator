bddf31aad132ee59fdd1b2fe39eba69edd0cc0a6
riemervdzee <riemervdzee@gmail.com>
2012-12-04 22:52:26 +0100

    Some code for the SimModel already

--- Source/TrafficController/cSimulationModel.cpp
+++ Source/TrafficController/cSimulationModel.cpp
@@ -3,6 +3,7 @@
 #include "cSimulationModel.h"
 #include "iNetworkObserver.h"
 #include <sstream>
+using namespace std;
 
 void cSimulationModel::Update()
 {
@@ -18,39 +19,54 @@ void cSimulationModel::UpdateNetwork()
 }
 
 void cSimulationModel::ProcessMessage(const Json::Value& data)
-{ 
+{
     // check which message we have
+    // Check for a starttime package (init pack)
     if(data.isMember("starttime"))
     {
-        std::stringstream sStream;
+        stringstream sStream;
         int hours = 0;
         int minutes = 0;
-        
+
         // init package
-        std::cout << "Starttime package" << std::endl;
-        
+        cout << "Starttime package" << endl;
+
         // parse time
-        std::string time = data["starttime"].asString();
+        string time = data["starttime"].asString();
         sStream << time.substr(0, 2);
         sStream >> hours;
         sStream.clear();
         sStream << time.substr(3, 2);
         sStream >> minutes;
-        
+
         // convert to seconds
         int timeSeconds = hours * 3600 + minutes * 60;
-        
+
         // DEBUG
-        std::cout << "H: " << hours * 3600 << "  M: " << minutes * 60 << std::endl;
-        
+        cout << "H: " << hours * 3600 << "  M: " << minutes * 60 << endl;
+
         // get the multiplier
         _Multiplier = data["multiplier"].asInt();
     }
+    // The received package is a light loop package
     else if(data.isMember("light"))
     {
+        // We find the empty = true packages quite boring
+        if ( data["empty"].asBool() == true)
+        {
+            cout << "We are too fly for empty=true packages" << endl;
+            return;
+        }
+
+
         // loop message
-        std::cout << "Loop package" << std::endl;
-        
+        cout << "Loop package" << endl;
+
         // do logic
+        cout << data["type"].asString() << endl;
+        cout << data["light"].asString() << endl;
+        cout << data["loop"].asString() << endl;
+        cout << data["empty"].asString() << endl;
+        cout << data["to"].asString() << endl;
     }
 }
